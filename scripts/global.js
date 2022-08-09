c985877b884629660f8ac5c7c31d9d85dd48f545
Lex Metcalfe <lex@gameaudio.ca>
2022-08-09 13:21:39 -0400

    Add files

---
+++ scripts/global.js
@@ -0,0 +1,25 @@
+// Add event listeners for clicks on nav items
+    document.getElementById("Home").addEventListener("click", load_home);
+    document.getElementById("Archive").addEventListener("click", load_archive);
+    document.getElementById("About").addEventListener("click", load_about);
+    document.getElementById("Contact").addEventListener("click", load_contact);
+
+// Load the home page
+    function load_home() {
+        document.getElementById("article").innerHTML = '<object type="text/html" data="home.html" width="100%" height="auto"></object>';
+    }
+
+// Load the archive page
+    function load_archive() {
+        document.getElementById("article").innerHTML = '<object type="text/html" data="archive.html" width="100%" height="auto"></object>';
+    }    
+
+// Load the about page
+    function load_about() {
+        document.getElementById("article").innerHTML = '<object type="text/html" data="about.html" width="100%" height="auto"></object>';
+    }        
+
+// Load the contact page
+    function load_contact() {
+        document.getElementById("article").innerHTML = '<object type="text/html" data="contact.html" width="100%" height="auto"></object>';
+    }   
