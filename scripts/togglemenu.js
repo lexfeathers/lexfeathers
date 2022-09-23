a0822d14725858f7a9fdf7e4534eae77a2afe452
Lex Metcalfe <lex@gameaudio.ca>
2022-09-23 15:37:24 -0400

    Trying to use the menu script in the index head

---
+++ scripts/togglemenu.js
@@ -0,0 +1,30 @@
+  // Menu toggling script
+  const menu = document.querySelector(".movablemenu");
+  const menuItems = document.querySelectorAll(".menuItem");
+  const hamburger= document.querySelector(".hamburger");
+  const closeIcon= document.querySelector(".closeIcon");
+  const menuIcon = document.querySelector(".menuIcon");
+
+  function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+      debug.log;
+    } else {
+      menu.classList.add("showMenu");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+      debug.log;
+    }
+  }
+
+  hamburger.addEventListener("click", toggleMenu);
+
+  // Hide menu if a menu item has been clicked
+  menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleMenu);
+      debug.log;
+    }
+  )  
