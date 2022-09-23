c1974f8be0f4decff4bfc695b79318d87226432a
Lex Metcalfe <lex@gameaudio.ca>
2022-09-23 16:34:07 -0400

    Noodling with the hamburger issue

--- scripts/togglemenu.js
+++ scripts/togglemenu.js
@@ -1,7 +1,7 @@
   // Menu toggling script
   const movablemenu = document.querySelector(".movablemenu");
   const menuItems = document.querySelectorAll(".menuItem");
-  const hamburger = document.querySelector(".hamburger");
+  const hamburger = document.querySelector(".hamburgerbutton");
   const closeIcon = document.querySelector(".closeIcon");
   const menuIcon = document.querySelector(".menuIcon");
 
