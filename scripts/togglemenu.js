4d05ea254a12438b5a4d0d5808ea306b8eefd7b8
Lex Metcalfe <lex@gameaudio.ca>
2022-09-23 15:41:41 -0400

    Fixed broken const declarations in menu toggling script

--- scripts/togglemenu.js
+++ scripts/togglemenu.js
@@ -1,8 +1,8 @@
   // Menu toggling script
   const menu = document.querySelector(".movablemenu");
   const menuItems = document.querySelectorAll(".menuItem");
-  const hamburger= document.querySelector(".hamburger");
-  const closeIcon= document.querySelector(".closeIcon");
+  const hamburger = document.querySelector(".hamburger");
+  const closeIcon = document.querySelector(".closeIcon");
   const menuIcon = document.querySelector(".menuIcon");
 
   function toggleMenu() {
