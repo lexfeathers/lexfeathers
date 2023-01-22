e4452b7c2176b75dd4660cc16b7d990f5a4b72a7
Lex Metcalfe <lex@gameaudio.ca>
2023-01-22 14:04:30 -0500

    Moving burger menu script to head

--- scripts/togglemenu.js
+++ scripts/togglemenu.js
@@ -1,27 +1,45 @@
-  // Menu toggling script
-  const movablemenu = document.querySelector("menuslider");
-  const menuItems = document.querySelectorAll(".menuItem");
-  const hamburger = document.querySelector("hamburgerbutton");
-  const closeIcon = document.querySelector(".closeIcon");
-  const menuIcon = document.querySelector(".menuIcon");
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu icon */
+  function toggleMobileMenu() {
 
-  function toggleMenu() {
-    if (movablemenu.classList.contains("showMenu")) {
-      movablemenu.classList.remove("showMenu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
+    var menuLinks = document.getElementById("menulinks");
+    var menuIcon = document.getElementById("menuIcon");
+    var closeIcon = document.getElementById("closeIcon");
+
+    if (menuLinks.style.display === "block") {
+        menuLinks.style.display = "none";
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
     } else {
-      movablemenu.classList.add("showMenu");
-      closeIcon.style.display = "block";
-      menuIcon.style.display = "none";
+        menuLinks.style.display = "block";
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
     }
-  }
+  } 
 
-  hamburger.addEventListener("click", toggleMenu);
+// // Menu toggling script
+  // const movablemenu = document.querySelector("menuslider");
+  // const menuItems = document.querySelectorAll(".menuItem");
+  // const hamburger = document.querySelector("hamburgerbutton");
+  // const closeIcon = document.querySelector(".closeIcon");
+  // const menuIcon = document.querySelector(".menuIcon");
+
+  // function toggleMenu() {
+  //   if (movablemenu.classList.contains("showMenu")) {
+  //     movablemenu.classList.remove("showMenu");
+  //     closeIcon.style.display = "none";
+  //     menuIcon.style.display = "block";
+  //   } else {
+  //     movablemenu.classList.add("showMenu");
+  //     closeIcon.style.display = "block";
+  //     menuIcon.style.display = "none";
+  //   }
+  // }
+
+  // hamburger.addEventListener("click", toggleMenu);
   
-  // Hide menu if a menu item has been clicked
-  menuItems.forEach( 
-    function(menuItem) { 
-      menuItem.addEventListener("click", toggleMenu);
-    }
-  )  
+  // // Hide menu if a menu item has been clicked
+  // menuItems.forEach( 
+  //   function(menuItem) { 
+  //     menuItem.addEventListener("click", toggleMenu);
+  //   }
+  // )  
